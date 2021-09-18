c3af14c3cc81fad697277e839d6939d025c3d7e9
fe-olaf <frontend.olaf@gmail.com>
2021-09-18 11:58:13 +0900

    prettier config 추가

--- src/js/type.js
+++ src/js/type.js
@@ -1,22 +1,22 @@
 /**
  * 1. 원시 타입
- * - String 
- * - Number 
- * - Boolean 
+ * - String
+ * - Number
+ * - Boolean
  * - Null (선언조차 되어지지 않은 상태)
  * - Undefined (값이 할당되어지지 않은 상태)
- * - Symbol 
- * 
+ * - Symbol
+ *
  * 2. 객체 타입
- * - function 
+ * - function
  * - array
  * - object
  */
 
 // 값을 할당하기 전에는 타입을 알 수 없다.
 // => 동적 타입 언어
-const s = 'string'  // string name = '홍길동' 
-const n = 10 
+const s = 'string' // string name = '홍길동'
+const n = 10
 const b = true
 const nu = null
 const u = undefined
@@ -25,19 +25,18 @@ const sy = Symbol()
 console.log(typeof s)
 console.log(typeof n)
 
-
 // 목적: 두 가지의 숫자를 받아서 더해주는 함수
 // const result = add(10, 20)
 // console.log(result) => 30
 function add(a, b) {
-   if (typeof a !== 'number' || typeof b !== 'number') {
-     console.log('숫자만 받을 수 있습니다.')
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    console.log('숫자만 받을 수 있습니다.')
 
-     // 함수는 return 을 만나면 역할을 끝낸다.
-     return 
-   }
+    // 함수는 return 을 만나면 역할을 끝낸다.
+    return
+  }
 
-   return a + b 
+  return a + b
 }
 
 const result = add(10, 20)
@@ -50,5 +49,3 @@ console.log('name', name)
 // const name: string = '홍길동'
 // function add(a:number, b:number): number {}
 // add('홍') => Error
-
-
